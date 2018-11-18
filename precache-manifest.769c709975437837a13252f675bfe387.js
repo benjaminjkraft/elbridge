cab9e14f128b2dd90eecf90c728ea312d27010f2
Ben Kraft <ben@benkraft.org>
2018-11-18 17:56:58 -0500

    Update of pages site

---
+++ precache-manifest.769c709975437837a13252f675bfe387.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dc2c6e25b16c73eb2f7",
+    "url": "/elbridge/static/js/runtime~main.0dc2c6e2.js"
+  },
+  {
+    "revision": "95d1107746b9cb56c0d1",
+    "url": "/elbridge/static/js/main.95d11077.chunk.js"
+  },
+  {
+    "revision": "1abe7352a0b011305622",
+    "url": "/elbridge/static/js/1.1abe7352.chunk.js"
+  },
+  {
+    "revision": "95d1107746b9cb56c0d1",
+    "url": "/elbridge/static/css/main.93a0c746.chunk.css"
+  },
+  {
+    "revision": "71e74a6b3988982ce8d285575ebe96de",
+    "url": "/elbridge/index.html"
+  }
+];
